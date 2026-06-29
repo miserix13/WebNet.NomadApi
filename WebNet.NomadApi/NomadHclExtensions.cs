9998c0a31d491892ecbb176e2656e53a3b936f61
JoJo Webster <151088710+miserix13@users.noreply.github.com>
2026-06-28 23:29:38 -0500

    Add Nomad HCL serialization extensions
    Introduce NomadHclExtensions with a ToHcl extension for NomadJob that serializes job, groups, and tasks into HCL using HclWriter. Implements helpers to emit group and task blocks, including task driver, config map entries, and resources (cpu/memory), enabling programmatic generation of Nomad HCL job specs.

---
+++ WebNet.NomadApi/NomadHclExtensions.cs
@@ -0,0 +1,61 @@
+﻿namespace WebNet.NomadApi
+{
+    public static class NomadHclExtensions
+    {
+        public static string ToHcl(this NomadJob job)
+        {
+            var hcl = new HclWriter();
+
+            hcl.Block("job", job.Name, () =>
+            {
+                hcl.Attribute("type", job.Type);
+
+                foreach (var group in job.Groups)
+                {
+                    WriteGroup(hcl, group);
+                }
+            });
+
+            return hcl.ToString();
+        }
+
+        private static void WriteGroup(HclWriter hcl, TaskGroup group)
+        {
+            hcl.Block("group", group.Name, () =>
+            {
+                hcl.Attribute("count", group.Count);
+
+                foreach (var task in group.Tasks)
+                {
+                    WriteTask(hcl, task);
+                }
+            });
+        }
+
+        private static void WriteTask(HclWriter hcl, NomadTask task)
+        {
+            hcl.Block("task", task.Name, () =>
+            {
+                hcl.Attribute("driver", task.Driver);
+
+                // config block
+                if (task.Config.Any())
+                {
+                    hcl.AppendLine("config {");
+                    foreach (var kv in task.Config)
+                    {
+                        var value = kv.Value is string s ? $"\"{s}\"" : kv.Value;
+                        hcl.AppendLine($"  {kv.Key} = {value}");
+                    }
+                    hcl.AppendLine("}");
+                }
+
+                // resources block
+                hcl.AppendLine("resources {");
+                hcl.AppendLine($"  cpu    = {task.Resources.Cpu}");
+                hcl.AppendLine($"  memory = {task.Resources.MemoryMb}");
+                hcl.AppendLine("}");
+            });
+        }
+    }
+}
